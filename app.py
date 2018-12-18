dd41fb677eb3da4563b6e337a774aa8e30ed13dd
wayoalamos <malamos2@uc.cl>
2018-12-18 20:42:09 -0300

    nothing much

--- app.py
+++ app.py
@@ -19,15 +19,15 @@ def homepage():
 
 @app.route("/getPlotCSV")
 def getPlotCSV():
-
+    """
     s = Search()
     url = 'https://www.portalinmobiliario.com/venta/casa/las-condes-metropolitana?ca=2&ts=1&mn=2&or=&sf=1&sp=0&at=0&pg='
     for i in range(4):
         a = str(i)
         print(a+"/156")
         url2 = url+a
-        s.find_products(url2)
-    csv = s.data
+        s.find_products(url2)"""
+    csv = "1,2,3\n"
     return Response(
         csv,
         mimetype="text/csv",
