84f420e7fbcb01fa989e0ad17b823e45d034d913
wayoalamos <malamos2@uc.cl>
2018-12-19 03:35:43 -0300

    js

--- search.py
+++ search.py
@@ -154,8 +154,7 @@ class Search:
         type = 0
         for elem in div.find_all("div", class_="col-sm-3"):
             for string in elem.stripped_strings:
-                # try:
-                if isinstance(string, str):
+                try:
                     string = string.decode("utf-8")
                 string = string.encode('utf-8').strip()
                 string = str(string)
