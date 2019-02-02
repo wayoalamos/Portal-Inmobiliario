8b9b00e8cec9034ca6639329f2964a0c91adf8a2
wayoalamos <malamos2@uc.cl>
2019-02-02 02:29:59 -0300

    .

--- app.py
+++ app.py
@@ -32,28 +32,10 @@ def getPlotCSV():
     result = first_job.result
     while not result:
         result = first_job.result
-        
+
     print("miiii resultado:", result)
 
-    s = Search() # create search element
-    s.workbook = Workbook() # create workbook element
-    s.workbook_active = s.workbook.active
-
-    # excel headers
-    header=["Tipo", "Categoria", "Ubicacion", "Codigo", "Informacion",
-    "Construido", "Terreno", "Valor(UF)", "UF/Construido", "UF/Terreno", "url"]
-    s.workbook_active.append(header)
-
-    # aca esta el problema
-    s.find_products(url) # find products of the urls
-    if s.status == 0:
-        # in case there was not enough time
-        print("here we go again!")
-        s.status = 1
-
-        # s.find_products(s.last_url)
-
-    output = make_response(openpyxl.writer.excel.save_virtual_workbook(s.workbook))
+    output = make_response(openpyxl.writer.excel.save_virtual_workbook(result))
     output.headers["Content-Disposition"] = "attachment; filename=PortalI.xlsx"
     output.headers["Content-type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 
--- utils.py
+++ utils.py
@@ -4,11 +4,22 @@ from flask import make_response
 import openpyxl
 import pyexcel
 
-import time
+from search import Search
+from openpyxl import Workbook
 
 
 def count_words_at_url(url):
     print("dddentro de la funcion")
-    time.sleep(5)
+    s = Search() # create search element
+    s.workbook = Workbook() # create workbook element
+    s.workbook_active = s.workbook.active
+
+    # excel headers
+    header=["Tipo", "Categoria", "Ubicacion", "Codigo", "Informacion",
+    "Construido", "Terreno", "Valor(UF)", "UF/Construido", "UF/Terreno", "url"]
+    s.workbook_active.append(header)
+
+    # aca esta el problema
+    s.find_products(url) # find products of the urls
     print("ttterminando la funcion")
-    return 12
+    return s.workbook
