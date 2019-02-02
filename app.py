4d9ce0cde062919c2d951a790db237c2398bcd11
wayoalamos <malamos2@uc.cl>
2019-02-02 02:08:21 -0300

    .

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, Response, request, render_template
+from flask import Flask, Response, request, render_template, make_response
 import flask_excel as excel
+from search import Search
+from openpyxl import Workbook
+import openpyxl
+import pyexcel
 
 from rq import Queue
 
 from worker import conn
 from search import Search
 from utils import count_words_at_url
+
+import time
 # fix dimensiones por la coma que hay se separan en dos valores distintos en el excel
 app = Flask(__name__)
 
@@ -18,13 +24,39 @@ def homepage():
 @app.route("/", methods=["POST"])
 def getPlotCSV():
     print("1111111")
+
+    q = Queue(connection=conn)
+    first_job = q.enqueue(count_words_at_url, url)
+    time.sleep(2)
+    result = first_job.result
+    print("miiii resultado:", result)
+
     text = request.form['text'] # url from webpage
     url = str(text)
-    q = Queue(connection=conn)
-    result = q.enqueue(count_words_at_url, url, app)
-    print("mi resultado:", result)
+    s = Search() # create search element
+    s.workbook = Workbook() # create workbook element
+    s.workbook_active = s.workbook.active
 
-    return "result"
+    # excel headers
+    header=["Tipo", "Categoria", "Ubicacion", "Codigo", "Informacion",
+    "Construido", "Terreno", "Valor(UF)", "UF/Construido", "UF/Terreno", "url"]
+    s.workbook_active.append(header)
+
+    # aca esta el problema
+    s.find_products(url) # find products of the urls
+    if s.status == 0:
+        # in case there was not enough time
+        print("here we go again!")
+        s.status = 1
+
+        # s.find_products(s.last_url)
+
+    output = make_response(openpyxl.writer.excel.save_virtual_workbook(s.workbook))
+    output.headers["Content-Disposition"] = "attachment; filename=PortalI.xlsx"
+    output.headers["Content-type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+
+
+    return output
 
 if __name__ == '__main__':
     excel.init_excel(app)
