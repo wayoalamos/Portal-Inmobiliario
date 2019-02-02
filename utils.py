ffefc0d5f0123ece5684995d343f5a0374da380e
wayoalamos <malamos2@uc.cl>
2019-02-02 00:00:12 -0300

    .

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import time
+
 def count_words_at_url():
     print("holaaa")
+    time.sleep(10)
+    print("hil de nuevo")
     return 12
--- worker.py
+++ worker.py
@@ -15,7 +15,6 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    print("soy un worker!! ")
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
